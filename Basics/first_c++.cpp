b29787ce5879f78a02f80fe970b95aa523445790
dibkb <dibas9110@gmail.com>
2023-08-31 18:29:07 +0530

    size

--- Basics/first_c++.cpp
+++ Basics/first_c++.cpp
@@ -2,7 +2,9 @@
 // pre-processor directive
 using namespace std;
 int main(){
-    cout << "Hello world" <<endl;
-    cout << "Today is 31st August";
+    // cout << "Hello world" <<endl;
+    // cout << "Today is 31st August";
+    long num = 18;
+    cout << sizeof num;
     return 0;
 }
