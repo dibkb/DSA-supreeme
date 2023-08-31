970887ef55c176c7959e45b0e9ed110488f03200
dibkb <dibas9110@gmail.com>
2023-08-31 10:42:22 +0530

    return 0

--- Basics/first_c++.cpp
+++ Basics/first_c++.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+// pre-processor directive
 using namespace std;
 int main(){
-    cout << "Hello world";
+    cout << "Hello world" <<endl;
+    cout << "Today is 31st August";
+    return 0;
 }
